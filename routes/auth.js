de7111883c2c8089b2c017a29bf720076485af2e
georgediepiriye <georgediepiriye4u@gmail.com>
2022-01-23 00:43:39 +0100

    worked on register endpoint

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const User = require("../models/User")
+
+const router = require("express").Router()
+const CryptoJS = require("crypto-js")
+
+//REGISTER
+router.post("/register",async(req,res)=>{
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password,process.env.PASS_SECRET ).toString()
+    })
+    try {
+        const savedUser = await newUser.save()
+        res.status(200).json(savedUser)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
+    
+})
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 const morgan = require('morgan')
 const userRouter = require("./routes/user")
+const authRouter = require("./routes/auth")
+
+
+
+app.use(express.json())
+
 
 //middleware for logging
 app.use(morgan('dev'))
 
+//routes
 app.use("/api/v1/users",userRouter);
+app.use("/api/v1/auth",authRouter);
 
-app.use(express.json())
 
 
 mongoose
