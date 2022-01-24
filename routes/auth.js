184c7d21268efab5a24ecda9095d40da115b6a3e
georgediepiriye <georgediepiriye4u@gmail.com>
2022-01-24 19:57:09 +0100

    worked on login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/User")
 
 const router = require("express").Router()
 const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
 
 //REGISTER
 router.post("/register",async(req,res)=>{
@@ -20,7 +21,40 @@ router.post("/register",async(req,res)=>{
     
 })
 
+//LOGIN
+router.post("/login",async(req,res)=>{
+    try {
+        const user = await User.findOne({username:req.body.username})
+        
+        !user && res.status(401).json("wrong credentials")
 
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SECRET);
+
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+        originalPassword!==req.body.password &&  res.status(401).json("wrong credentials")
+
+
+        const accessToken = jwt.sign({
+            id:user._id,
+            isAdmin: user.isAdmin
+        },
+        process.env.JWT_SEC,
+        {expiresIn:"3d"}
+        )
+
+        const {password, ...others}= user._doc
+
+
+
+        res.status(200).json({...others,accessToken})
+
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+})
 
 
 module.exports = router
